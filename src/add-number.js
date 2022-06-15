da8786a80af3c62c078d28cc85013946ac2eab3f
alihaidermalik20 <95946965+alihaidermalik20@users.noreply.github.com>
2022-06-15 15:20:06 +0200

    addNumber and listItems functions update

--- src/add-number.js
+++ src/add-number.js
@@ -1,6 +1,6 @@
 import { numbers } from '../numbers.js';
 
-import { readNumber, display } from '../lib/dom-io.js';
+import { readNumber, display, append } from '../lib/dom-io.js';
 
 import { listItems } from './utils/list-items.js';
 
@@ -10,11 +10,15 @@ const addNumber = () => {
   debugger;
 
   // read the user's number
+  const inputNum = readNumber('the-num');
 
   // add it to the array of numbers
+  numbers.push(inputNum);
+  console.log(numbers);
 
   // generate and display a string listing all numbers
+  display('your-output', listItems(numbers));
 };
 
 // !! fill in this blank with the correct DOM ID
-document.getElementById('__').addEventListener('click', addNumber);
+document.getElementById('add-it').addEventListener('click', addNumber);
